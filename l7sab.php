cebfe7af76ee52e259b95f4d1a5d701d379edeb4
yassplays-rgb <yassinehsain5@gmail.com>
2025-03-23 01:49:00 +0000

    first commit

---
+++ l7sab.php
@@ -0,0 +1,34 @@
+<?php
+    $file = "E:\Lma7al\Mars.txt";
+    $of = fopen($file,"r");
+    $ligne = "";
+    $prixA = 0;
+    $prixV = 0;
+    $benefice = 0;
+    while(($ligne = fgets($of)) !== false){
+            $table = explode(':',$ligne);
+            $prixA += (float)$table[1];
+            $prixV += (float)$table[2];
+            $benefice += (float)$table[3];
+    }
+    echo "Votre achats au total est : " . $prixA ;
+    echo "\n Votre Total de ventes est : " . $prixV;
+    echo "\n Votre benefice total est : " . $benefice;
+?>
+
+<!--
+nom:prixA:prixV:benefice
+
+RM = reparation Manette
+RDJ = recharge des jeux
+RW = reparation whatsapp
+
+$c = fgetc($of);
+        if($c == "\n"){
+            $table = explode(':',$ligne);
+            $prixA += (float)$table[1];
+            $prixV += (float)$table[2];
+            $benefice += (float)$table[3];
+        } else {
+            $ligne .= $c;
+        }
