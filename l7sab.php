438d7e9124152a1c5f0c58b150f158db88022aa2
yassplays-rgb <yassinehsain5@gmail.com>
2025-03-30 23:51:17 +0000

    autres ventes

--- l7sab.php
+++ l7sab.php
@@ -15,20 +15,3 @@
     echo "\n Votre Total de ventes est : " . $prixV;
     echo "\n Votre benefice total est : " . $benefice;
 ?>
-
-<!--
-nom:prixA:prixV:benefice
-
-RM = reparation Manette
-RDJ = recharge des jeux
-RW = reparation whatsapp
-
-$c = fgetc($of);
-        if($c == "\n"){
-            $table = explode(':',$ligne);
-            $prixA += (float)$table[1];
-            $prixV += (float)$table[2];
-            $benefice += (float)$table[3];
-        } else {
-            $ligne .= $c;
-        }
