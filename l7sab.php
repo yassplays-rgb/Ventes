9c0b56f9cea4e81d27fee7a18a545645a823a8f3
yassplays-rgb <yassinehsain5@gmail.com>
2025-04-05 19:51:34 +0000

    Nouveau mois (avril) + des ventes

--- l7sab.php
+++ l7sab.php
@@ -1,17 +1,27 @@
 <?php
-    $file = "Mars.txt";
-    $of = fopen($file,"r");
-    $ligne = "";
-    $prixA = 0;
-    $prixV = 0;
-    $benefice = 0;
-    while(($ligne = fgets($of)) !== false){
-            $table = explode(':',$ligne);
-            $prixA += (float)$table[1];
-            $prixV += (float)$table[2];
-            $benefice += (float)$table[3];
+    $fileMars = "Mars.txt";
+    $fileAvril = "Avril.txt";
+
+    function CalculerFile($file){
+        $of = fopen($file,"r");
+        $ligne = "";
+        $prixA = 0;
+        $prixV = 0;
+        $benefice = 0;
+        while(($ligne = fgets($of)) !== false){
+                $table = explode(':',$ligne);
+                $prixA += (float)$table[1];
+                $prixV += (float)$table[2];
+                $benefice += (float)$table[3];
+        }
+        echo "Votre achats au total est : " . $prixA ;
+        echo "\n Votre Total de ventes est : " . $prixV;
+        echo "\n Votre benefice total est : " . $benefice;
+        $nette = $benefice - 2617;
+        echo "\n Nette = " .$nette ;
     }
-    echo "Votre achats au total est : " . $prixA ;
-    echo "\n Votre Total de ventes est : " . $prixV;
-    echo "\n Votre benefice total est : " . $benefice;
+
+    CalculerFile($fileMars);
+    echo("\n\n\n\n");
+    CalculerFile($fileAvril);
 ?>
